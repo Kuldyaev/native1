d39f0c7fc454a605b16785bfee76115ac53fed42
Kuldyaev <slavalion1@gmail.com>
2023-09-25 19:29:49 +0300

    list of categories in shopping list

--- components/shopListActive.js
+++ components/shopListActive.js
@@ -1,25 +1,52 @@
-import { View,  Text, } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 
-
-
-
-
-function ShopListActive({list}) {
+function ShopListActive({ list }) {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.shopList.categories);
-  
 
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+  const [categoriesBase, setCategoriesBase ]= useState([]);
 
-      <Text>Active</Text>
+  useEffect(() => {
+    const newCatsBase = [];
+    if (categories.length > 0) {
+      categories.forEach((element) => {
+        newCatsBase[element] = {
+          catname: element,
+          hide: false,
+          items: [],
+        };
+      });
+      
+      list.forEach((item) => {
+        newCatsBase[item.category_name].items.push(item);
+      });
+      setCategoriesBase(newCatsBase);
+      console.log(newCatsBase);
+    }
+  },[categories]);
 
-      <Text>{list.length}</Text>
-      <Text>{categories[0]}</Text>
-       
-    </View>
-  );
+  useEffect(()=>{
+    console.log(categoriesBase);
+  }, [categoriesBase])
+      
+
+  if (categories.length > 0) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Active</Text>
+        <FlatList data={categories} renderItem={({item})=>(<Text>{item}</Text>)} />
+      </View>
+    );
+  } else {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Active</Text>
+        <Text>empty</Text>
+      </View>
+    );
+  }
 }
 
 export default ShopListActive;
